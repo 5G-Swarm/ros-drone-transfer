6d550f88f7bc0038bd02e0f262f91c6085897f4e
shahao <65599452+SteveHao74@users.noreply.github.com>
2022-04-21 23:59:09 +0800

    drone syn

--- robot.py
+++ robot.py
@@ -3,12 +3,11 @@
 import cv2
 import numpy as np
 import time
-
+import message_filters
 import rospy
-from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import Image, BatteryState, Imu,NavSatFix
-from mavros_msgs.msg import Altitude#, EstimatorStatus
+# from mavros_msgs.msg import Altitude#, EstimatorStatus
 from geometry_msgs.msg import TwistStamped
 from nav_msgs.msg import Odometry
 
@@ -24,10 +23,42 @@ class Client(Informer):
     def cmd_recv(self):
         self.recv('cmd', parse_cmd)
 
-def callback_gps_pos(ros_gps_pos):
+
+def callback_syn(ros_img,ros_gps_pos,ros_gps_vel,ros_imu):
+    global ifm
+    img = np.ndarray(shape=(480, 640, 3), dtype=np.dtype("uint8"), buffer=ros_img.data)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    _, jpeg = cv2.imencode('.jpg', img)
+    data = jpeg.tobytes()
+    ifm.send_img(data)
+
     _ = ros_gps_pos.altitude
     _ = ros_gps_pos.latitude
     _ = ros_gps_pos.longtitude
+
+    _ = ros_gps_vel.twist.linear.x
+    _ = ros_gps_vel.twist.linear.y
+    _ = ros_gps_vel.twist.linear.z
+
+    _ = ros_imu.orientation.x
+    _ = ros_imu.orientation.y
+    _ = ros_imu.orientation.z
+    _ = ros_imu.orientation.w
+    
+    _ = ros_imu.angular_velocity.x
+    _ = ros_imu.angular_velocity.y
+    _ = ros_imu.angular_velocity.z
+
+
+
+
+
+    
+
+# def callback_gps_pos(ros_gps_pos):
+#     _ = ros_gps_pos.altitude
+#     _ = ros_gps_pos.latitude
+#     _ = ros_gps_pos.longtitude
     
 
 
@@ -36,21 +67,21 @@ def callback_battery(ros_battery):
     _ = ros_battery.percentage
 
 
-def callback_gps_vel(ros_gps_vel):
-    _ = ros_gps_vel.twist.linear.x
-    _ = ros_gps_vel.twist.linear.y
-    _ = ros_gps_vel.twist.linear.z
+# def callback_gps_vel(ros_gps_vel):
+#     _ = ros_gps_vel.twist.linear.x
+#     _ = ros_gps_vel.twist.linear.y
+#     _ = ros_gps_vel.twist.linear.z
 
     
-def callback_imu(ros_imu):
-    _ = ros_imu.angular_velocity.x
-    _ = ros_imu.angular_velocity.y
-    _ = ros_imu.angular_velocity.z
+# def callback_imu(ros_imu):
+#     _ = ros_imu.angular_velocity.x
+#     _ = ros_imu.angular_velocity.y
+#     _ = ros_imu.angular_velocity.z
 
-    _ = ros_imu.orientation.x
-    _ = ros_imu.orientation.y
-    _ = ros_imu.orientation.z
-    _ = ros_imu.orientation.w
+#     _ = ros_imu.orientation.x
+#     _ = ros_imu.orientation.y
+#     _ = ros_imu.orientation.z
+#     _ = ros_imu.orientation.w
 
     # _ = imu.linear_acceleration.x
     # _ = imu.linear_acceleration.y
@@ -74,13 +105,13 @@ def callback_odom(ros_odom):
     _ = ros_odom.twist.twist.linear.y
     _ = ros_odom.twist.twist.linear.z
 
-def callback_img(ros_img):
-    global ifm
-    img = np.ndarray(shape=(480, 640, 3), dtype=np.dtype("uint8"), buffer=ros_img.data)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    _, jpeg = cv2.imencode('.jpg', img)
-    data = jpeg.tobytes()
-    ifm.send_img(data)
+# def callback_img(ros_img):
+#     global ifm
+#     img = np.ndarray(shape=(480, 640, 3), dtype=np.dtype("uint8"), buffer=ros_img.data)
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#     _, jpeg = cv2.imencode('.jpg', img)
+#     data = jpeg.tobytes()
+#     ifm.send_img(data)
     # cv2.imshow('img', img)
     # cv2.waitKey(2)
 
@@ -88,12 +119,16 @@ if __name__ == '__main__':
     ifm = Client(config = 'config.yaml')
 
     rospy.init_node('drone_5g_transfer', anonymous=True)
-    rospy.Subscriber('/camera/color/image_raw', Image, callback_img)
-    rospy.Subscriber('/mavros/global_position/global', NavSatFix, callback_gps_pos)
-    rospy.Subscriber('/mavros/battery', BatteryState, callback_battery)
-    rospy.Subscriber('/mavros/global_position/raw/gps_vel', TwistStamped, callback_gps_vel)
-    rospy.Subscriber('/mavros/imu/data', Imu, callback_imu)
+    sub_camera = message_filters.Subscriber('/camera/color/image_raw', Image, callback_img)
+    sub_gps = message_filters.Subscriber('/mavros/global_position/global', NavSatFix, callback_gps_pos)
+    sub_gps_vel = message_filters.Subscriber('/mavros/global_position/raw/gps_vel', TwistStamped, callback_gps_vel)
+    sub_imu = message_filters.Subscriber('/mavros/imu/data', Imu, callback_imu)
+    sync_listener = message_filters.ApproximateTimeSynchronizer([sub_camera,sub_gps,sub_gps_vel,sub_imu], 10, 1, allow_headerless=True)
+    sync_listener.registerCallback(callback)
+
     rospy.Subscriber('/mavros/local_position/odom', Odometry, callback_odom)
+    rospy.Subscriber('/mavros/battery', BatteryState, callback_battery)
+    
 
     # rospy.spin()
     rate = rospy.Rate(1000)
